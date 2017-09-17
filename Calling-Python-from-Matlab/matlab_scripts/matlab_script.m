3d253bc74d9e55f9d49a03b0d22a6ba778fff408
Dhrubojyoti Roy <ineda@inedasystems.com>
2017-09-17 17:15:35 -0400

    Adding test module Calling-Python-from-Matlab. Updating .gitignore to exclude __pycache__

---
+++ Calling-Python-from-Matlab/matlab_scripts/matlab_script.m
@@ -0,0 +1,10 @@
+[own_path, ~, ~] = fileparts(mfilename('fullpath'));
+module_path = fullfile(own_path, '..');
+python_path = py.sys.path;
+if count(python_path, module_path) == 0
+    insert(python_path, int32(0), module_path);
+end
+
+py.our_module.our_script.our_function('Hello')
+py.our_module.our_script.array_func([1 2 3 4 5]);
+py.our_module.our_script.matrix_func('/home/roy.174/Documents/MATLAB/testmatrix.mat');
---
+++ Calling-Python-from-Matlab/our_module/our_script.py
@@ -0,0 +1,18 @@
+import numpy
+import scipy.io as sio
+
+def our_function(text):
+    print('Test line')
+    print('%s %f' % (text, numpy.nan))
+
+def array_func(arr):
+    print('Array: ', arr)
+    print('Mean: ', numpy.mean(arr))
+    print('Median: ', numpy.median(arr))
+
+def matrix_func(matfile):
+    matrix = sio.loadmat(matfile)
+    print('Passed matrix: ', matrix)
+    print('Inspecting matrix: ', sio.whosmat(matfile))
+    print('Matrix mean: ', numpy.mean(matrix.mat))
+    print('Matrix median: ', numpy.median(matrix.mat))
