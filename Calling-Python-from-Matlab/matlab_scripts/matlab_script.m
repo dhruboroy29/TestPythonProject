121af78f0e56acf68d04860e51a653ae591c5564
Dhrubojyoti Roy <ineda@inedasystems.com>
2017-09-18 10:18:05 -0400

    Minor update (added ;)

--- Calling-Python-from-Matlab/matlab_scripts/matlab_script.m
+++ Calling-Python-from-Matlab/matlab_scripts/matlab_script.m
@@ -5,6 +5,6 @@ if count(python_path, module_path) == 0
     insert(python_path, int32(0), module_path);
 end
 
-py.our_module.our_script.our_function('Hello')
+py.our_module.our_script.our_function('Hello');
 py.our_module.our_script.array_func([1 2 3 4 5]);
 py.our_module.our_script.matrix_func('../testmatrix.mat');
