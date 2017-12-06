038710096c1d458aa7c37b905efdaaf8620cc92f
Dhrubojyoti Roy <royd@cse.osu.edu>
2017-12-06 14:04:24 -0500

    Using package pandas, scipy.io.arff, sklearn.preprocessing.LabelEncoder and sklearn.preprocessing.OneHotEncoder to demonstrate One-Hot encoding for iris.arff and radar1_scaled.arff

--- TextLabelsToOneHot.py
+++ TextLabelsToOneHot.py
@@ -1,20 +1,75 @@
-import numpy as np, arff
-from sklearn.preprocessing import LabelBinarizer
+import numpy as np, pandas as pd
+from scipy.io import arff
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-dataset = arff.load(open('iris.arff', 'r'))
-data = np.array(dataset['data'])
-text_labels = data[:,4]
+print("###################################")
+print("#One-Hot Encoding Demo - iris.arff#")
+print("###################################")
 
-print("#######################")
-print("#One-Hot Encoding Demo#")
-print("#######################")
+# Load dataset1
+dataset1 = arff.loadarff('iris.arff')
+
+# Extract data
+df = pd.DataFrame(dataset1[0])
+data1 = df.select_dtypes(exclude=[object]).as_matrix()
+
+# Extract labels
+lab1 = df.select_dtypes(include=[object])
+
+# How many labels?
+num_labels = lab1.size
+
+# How many classes?
+num_classes = np.unique(lab1).size
 
 # Print original labels
-print("Text labels: %s\n" % text_labels)
+print("Text labels: %s\n" % np.unique(lab1))
 
-# Encode labels
-encoder = LabelBinarizer()
-encoded_labels = encoder.fit_transform(text_labels)
+# Encode labels as numbers
+encoder = LabelEncoder()
+encoded_labels = lab1.apply(encoder.fit_transform)
+
+# Convert to one-hot
+onehot = OneHotEncoder()
+onehot.fit(encoded_labels)
+onehot_labels = onehot.transform(encoded_labels).toarray()
 
 # Print one-hot labels
-print("One-Hot labels: %s\n" % encoded_labels)
+print("One-Hot labels: %s\n" % onehot_labels)
+
+#--------------------------------------------------------------------------------------------------------------#
+
+print("############################################")
+print("#One-Hot Encoding Demo - radar1_scaled.arff#")
+print("############################################")
+
+# Load dataset2
+dataset2 = arff.loadarff('radar1_scaled.arff')
+
+# Extract data
+df = pd.DataFrame(dataset2[0])
+data2 = df.select_dtypes(exclude=[object]).as_matrix()
+
+# Extract labels
+lab2 = df.select_dtypes(include=[object])
+
+# How many labels?
+num_labels = lab2.size
+
+# How many classes?
+num_classes = np.unique(lab1).size
+
+# Print original labels
+print("Text labels: %s\n" % np.unique(lab2))
+
+# Encode labels as numbers
+encoder = LabelEncoder()
+encoded_labels = lab2.apply(encoder.fit_transform)
+
+# Convert to one-hot
+onehot = OneHotEncoder()
+onehot.fit(encoded_labels)
+onehot_labels = onehot.transform(encoded_labels).toarray()
+
+# Print one-hot labels
+print("One-Hot labels: %s\n" % onehot_labels)
